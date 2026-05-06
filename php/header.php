691dca3541461744098a4495e4c023510754086d
nickyzinn16 <rickydavidbans27@gmail.com>
2026-05-06 18:47:33 -0100

    Header e Footer

--- php/header.php
+++ php/header.php
@@ -1,24 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
-    <header class="header">
-        <nav class="navbar">
-            <div class="logo">
-                <a href="index.html">
-                    <img alt="Logo GEIPE" src="./resources/images/logo-geipe.png">
-                </a>
-            </div>
+<html lang="pt">
+    <head>
+        <meta charset="UTF-8">
+        <title>GEIPE</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
 
-            <div class="menu">
-                <ul>
-                    <li><a href="index.html" class="ativo">Início</a></li>
-                    <li><a href="estagios.html">Estágios</a></li>
-                    <li><a href="insercao.html">Inserção Profissional</a></li>
-                    <li><a href="empreendedorismo.html">Empreendedorismo</a></li>
-                    <li><a href="contactos.html">Contacta-nos</a></li>
-                    <li><a href="blog.html">Blog</a></li>
-                    <li><a href="login.html" class="btn-login">Login</a></li>
-                </ul>
-            </div>
-        </nav>
-    </header>
-    
+    chamar o header2 aqui
---
+++ php/header2.php
@@ -0,0 +1,21 @@
+<header class="header">
+        <nav class="navbar">
+            <div class="logo">
+                <a href="index.php">
+                    <img alt="Logo GEIPE" src="./resources/images/logo-geipe.png">
+                </a>
+            </div>
+
+            <div class="menu">
+                <ul>
+                    <li><a href="index.php" class="ativo">Início</a></li>
+                    <li><a href="estagios.php">Estágios</a></li>
+                    <li><a href="insercao.php">Inserção Profissional</a></li>
+                    <li><a href="empreendedorismo.php">Empreendedorismo</a></li>
+                    <li><a href="contactos.php">Contacta-nos</a></li>
+                    <li><a href="blog.php">Blog</a></li>
+                    <li><a href="login.php" class="btn-login">Login</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
