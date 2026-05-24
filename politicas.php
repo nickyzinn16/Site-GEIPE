1dec38aef7e222a89e78a4bef42a2c4ee24f710d
Iran Silva <iransilvaasibd@gmail.com>
2026-05-24 14:27:04 -0100

    Atualizacoes 24/05/2026 - paginav de politicas e privacidade

--- politicas.php
+++ politicas.php
@@ -12,5 +12,16 @@
         <p>Saiba mais sobre as políticas de segurança da GEIPE</p>
     </div>
 </section>
+<section class="conteudo">
+    <div class="container-politicas">
+        <h1>Políticas de Privacidade</h1><br>
+        <h2>Estas são as políticas de privacidade da GEIPE. Ao utilizar a nossa plataforma, você concorda com os termos e as condições descritas nesta política.</h2><br>
+        <p>O funcionamento da plataforma digital do Gabinete de Estágios, Inserção Profissional e Empreendedorismo (GEIPE) da Escola Industrial e Comercial do Mindelo rege-se por normas estritas de segurança e confidencialidade, estabelecendo com clareza como as informações dos utilizadores são recolhidas, processadas e garantindo assim a proteção das mesmas. Alinhado com as necessidades pedagógicas e institucionais do Gabinete, o sistema processa dados meramente operacionais e necessários para as suas funcionalidades regulamentares. A recolha inicia-se a partir da interação voluntária dos visitantes sejam eles alunos, formandos ou docentes, que submetem os seus dados pessoais através dos formulários de contacto, ou de candidatura a vagas de estágio curricular e profissional.</p><br>
+        <p>Todas as informações coletadas, que incluem o nome completo, a área escolar e o endereço de e-mail do candidato, são integradas diretamente na base de dados local. O armazenamento destas informações é estruturado de forma consistente e segura nas tabelas do sistema, como as de candidaturas e contactos, utilizando restrições técnicas que impedem a duplicação ,alteração, no geral a corrupção dos dados. Adicionalmente, o sistema processa e armazena de forma crítica as credenciais de acesso para perfis autorizados de gestores e administradores do gabinete, garantindo que apenas o pessoal técnico validado tenha permissão para gerir ofertas de emprego, responder às perguntas frequentes (FAQ) e ou publicar avisos institucionais no blog da plataforma.</p><br>
+        <p>A segurança da informação é um requisito crítico para nós do GEIPE. Por conseguinte, os dados de identificação e os registos de comunicação localizados nos servidores locais da instituição não são partilhados, cedidos ou comercializados com entidades externas, exceto sob consulta legal ou institucional. No âmbito da comunicação direta, a plataforma reserva-se o direito de utilizar o e-mail fornecido para enviar notificações administrativas e alertas de novas vagas aos utilizadores. O utilizador retém total autonomia sobre os seus dados, sendo-lhe dado o direito de solicitar a consulta, atualização ou remoção dos seus registos diretamente junto aos administradores do sistema, consolidando um ambiente tecnológico seguro, transparente e em conformidade com as boas práticas de desenvolvimento de software.</p><br>
+        <p>Em resumo, a política de privacidade do GEIPE é um compromisso firme com a proteção dos dados dos utilizadores, assegurando que as informações pessoais sejam tratadas com o mais alto nível de segurança e confidencialidade, alinhada com as necessidades operacionais e pedagógicas do gabinete.</p><br>
+        <h2>Desde já muito obrigado(a) por escolher o GEIPE!</h2>
+    </div>
+</section>
 
 <?php include 'php/footer.php'; ?>
